3d662ddba6b8e68fa937d72519f23f9a07f6718b
CallumTanner <113216876+CallumTanner@users.noreply.github.com>
2022-09-25 18:30:49 +0100

    added js

--- script.js
+++ script.js
@@ -5,6 +5,29 @@
    ===================
 */
 
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('submit').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_ol = document.createElement('ol');
+  new_ol.innerText = getNumberOrString(document.getElementById('posted').value);
+
+  element_list.appendChild(new_ol);
+
+});
+
+
+
 const {
   core: { test, expect, run },
   prettify
