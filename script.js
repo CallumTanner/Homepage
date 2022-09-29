feec648dbcafd35b3772faabc4075a2f6be0dc92
CallumTanner <113216876+CallumTanner@users.noreply.github.com>
2022-09-29 21:29:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,90 @@
-function getNumberOrString(value) {
-  // Convert a string value to a number if possible
-  let number_value = Number(value);
-  if (Number.isNaN(number_value)) {
-    return value
-  } else {
-    return number_value
-  }
-}
+/* =================
+  TESTS, LOOK AT THESE
+  Reading tests will always help you discover your requirements.
+  You can make this window bigger. 
+   ===================
+*/
+
+var images;
 
 
+images = ['https://blog.codeanalogies.com/wp-content/uploads/h5p/content/2/images/imageAfter-5aefc26211941.png', 'https://qph.cf2.quoracdn.net/main-qimg-6795e6ef1ca101a38e86eee75ed1189f-lq', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIcr-fNsE9Za0kVA9MFp4SgQz-dI-9hWezuQ&usqp=CAU'];
+let element_image = document.getElementById('image');
+element_image.setAttribute("src", images[0]);
 
-document.getElementById('submit').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  let new_ol = document.createElement('ol');
-  new_ol.innerText = getNumberOrString(document.getElementById('posted').value);
 
-  element_list.appendChild(new_ol);
+document.getElementById('previous').addEventListener('click', (event) => {
+  let element_image2 = document.getElementById('image');
+  images.unshift(images.pop());
+  element_image2.setAttribute("src", images[0]);
 
 });
 
+document.getElementById('next').addEventListener('click', (event) => {
+  let element_image3 = document.getElementById('image');
+  images.push(images.shift());
+  element_image3.setAttribute("src", images[0]);
 
+});
 
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
 
+/* =================
+  FIND ELEMENTS
+  These are all the elements we will look for.
+   ===================
+*/
+const getHeader = document.querySelectorAll("header"),
+  getH1 = document.querySelectorAll("h1"),
+  getSiteHeader = document.querySelectorAll(".c-site-header"),
+  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+  getMain = document.querySelectorAll("main"),
+  getFooter = document.querySelectorAll("footer"),
+  getSiteFooter = document.querySelectorAll(".c-site-footer"),
+  getIFrame = document.querySelectorAll("iframe"),
+  getImage = document.querySelectorAll("img"),
+  getWords = document.body.innerText;
 
+/* =================
+   ASSERTIONS 
+   These are the things we check are true about your page.
+   Read and update your HTML to discover the requirements.
+   The tests will run every time you update your code.
+   ===================
+*/
+test("There is at least one header element", () => {
+  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one h1", () => {
+  expect(getH1.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one header element with the class c-site-header", () => {
+  expect(getSiteHeader.length).toBe(1);
+});
+test("There is a nav element with an aria-label of Main Site Links.", () => {
+  expect(getAria.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one main element", () => {
+  expect(getMain.length).toBe(1);
+});
+test("There is at least one footer element", () => {
+  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one footer element with the class c-site-footer", () => {
+  expect(getSiteFooter.length).toBe(1);
+});
+test("There is embedded video", () => {
+  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one image", () => {
+  expect(getImage.length).toBeGreaterThanOrEqual(1);
+});
+test("There are at least 500 words on the page", () => {
+  expect(getWords.length).toBeGreaterThanOrEqual(500);
+});
 
-
+const console = document.getElementById("tests");
+prettify.toHTML(run(), console);
